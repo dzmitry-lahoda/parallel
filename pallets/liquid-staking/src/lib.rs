69028473b77b9f64f392c8baa3ac288d1851ecc3
dzmitry-lahoda <dzmitry@lahoda.pro>
2023-11-06 15:54:44 +0000

    docs(liquid-staking): comments to pallets operation

--- pallets/liquid-staking/src/lib.rs
+++ pallets/liquid-staking/src/lib.rs
@@ -17,6 +17,31 @@
 //! ## Overview
 //!
 //! This pallet manages the NPoS operations for relay chain asset.
+//! General operation allows user to stake relay `staking token`s and get `derivative token`.
+//! Derivate token later used for unstaking. 
+//! 
+//! Requires general understanding of relay staking https://wiki.polkadot.network/docs/learn-staking-index
+//! 
+//! ## From user perspective
+//! 
+//! When user stakes, next happens:
+//! - staking token, after some fees deduction, gets into accounting of this pallet
+//! - user is minted, after ration conversion, with derivative token
+//! 
+//! When user unstakes,
+//! - his derived token is burn
+//! - his unstake request recorded
+//! 
+//! Later on, user claims his staked token with accrued rewards.
+//! 
+//! If unstake did not happened yet, user can cancel his request if he used fast unstake.
+//! Fast unstake matches user who want to stake with users who want to unstake inside pallet.
+//! 
+//! ## From operation perspective
+//! 
+//! Era and block of relay are delivered to this pallet in trustless way.
+//! 
+//! Fast unstaking requests (in this era) reduce ratio of staking to derived token, deincetivicing to unstake.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
